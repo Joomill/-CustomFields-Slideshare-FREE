af3fcbb1aeb2f995e4674d4ec69fcf5e49441f62
Joomill <info@joomill.nl>
2020-06-01 23:49:11 +0200

    V1.2.0 - Fixes

--- tmpl/slideshare.php
+++ tmpl/slideshare.php
@@ -11,12 +11,10 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
 
-
 //add stylesheet for responsive container
 $document = Factory::getDocument();
 $document->addStylesheet('plugins/fields/slideshare/tmpl/style.css');
 
-//get parameters
 $value = $field->value;
 $width = $fieldParams->get('width','100%');
 $height = $fieldParams->get('height','300px');
@@ -29,23 +27,24 @@ $embedParts = explode("&", $tidiedoriginal);
 
 //Make sure trailing ] is to be removed from new type embed url without &w=425, and still keep backward compatibility of older embeds
 if(isset($embedParts[1])){
-$embedtidyup = explode("]", $embedParts[1]);
+	$embedtidyup = explode("]", $embedParts[1]);
 }
 //Isolate unique id by removing [slideshare id= and store in $cleaned_id
-$cleaned_id = explode("=", $embedParts[0]);
+	$cleaned_id = explode("=", $embedParts[0]);
 
 //Unique id is same as isolated cleaned
 if(isset($cleaned_id[1])){
-$cleaned_id2 = $cleaned_id[1];
+	$cleaned_id2 = $cleaned_id[1];
 }
 else {
-$cleaned_id2 = $cleaned_id;
+	$cleaned_id2 = $cleaned_id;
 }
 
 if ($value == '')
 {
 	return;
 }
-echo '<div align="left" class="responsive-container" id="ss_'. $cleaned_id2.'">
-<iframe  style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" src="//www.slideshare.net/slideshow/embed_code/'. $cleaned_id2.'?width="'.$width.'" height="'.$height.'"  frameborder="0" marginwidth="0" marginheight="0" scrolling="no"> </iframe>
+
+echo '<div align="left" id="ss_'. $cleaned_id2.'">
+<iframe  style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" src="//www.slideshare.net/slideshow/embed_code/'. $cleaned_id2.'" width="'.$width.'" height="'.$height.'"  frameborder="0" marginwidth="0" marginheight="0" scrolling="no"> </iframe>
 </div>';
