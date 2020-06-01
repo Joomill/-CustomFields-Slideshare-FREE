f3a20b044775e55e1952493003465e3d1f59aa17
Joomill <info@joomill.nl>
2020-06-01 20:39:16 +0200

    V1.2.0 - Compatible with Joomla 4 Beta

--- script.php
+++ script.php
@@ -1,20 +1,21 @@
 <?php
 /**
- * Fields - Slideshare plugin for Joomla
- *
- * @author Joomill (info@joomill-extensions.com)
- * @copyright Copyright (c) 2017 Joomill
- * @license GNU Public License
- * @link https://www.joomill-extensions.com/
+ *  package: Custom Fields - Slideshare plugin - FREE Version
+ *  copyright: Copyright (c) 2020. Jeroen Moolenschot | Joomill
+ *  license: GNU General Public License version 3 or later
+ *  link: https://www.joomill-extensions.com
  */
 
+// No direct access.
 defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
 
 class plgFieldsSlideshareInstallerScript
 {
 	public function install($parent)
 	{
 		jimport('joomla.filesystem.file');
-        JFactory::getDBO()->setQuery("UPDATE `#__extensions` SET `enabled` = 1 WHERE `type` = 'plugin' AND`element` = 'slideshare'")->execute();
+        Factory::getDBO()->setQuery("UPDATE `#__extensions` SET `enabled` = 1 WHERE `type` = 'plugin' AND`element` = 'slideshare'")->execute();
 	}
 }
