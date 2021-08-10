a5d2f4f8fe65b5518dc7a1134eb7d52ab73a35ce
Joomill <info@joomill.nl>
2021-08-10 20:05:47 +0200

    V4.0.0 - First Commit

--- slideshare.php
+++ slideshare.php
@@ -19,31 +19,5 @@ JLoader::import('components.com_fields.libraries.fieldsplugin', JPATH_ADMINISTRA
  * @since  1.0.0
  */
 class PlgFieldsSlideshare extends FieldsPlugin
-
 {
-	public function onInstallerBeforePackageDownload(&$url, &$headers)
-	{
-		$uri = URI::getInstance($url);
-		
-		// I don't care about download URLs not coming from our site
-		// Note: as the Download ID is common for all extensions, this plugin will be triggered for all
-		// extensions with a download URL on our site
-		$host = $uri->getHost();
-		if (!in_array($host, array('www.joomill-extensions.com', 'joomill-extensions.com')))
-		{
-			return true;
-		}
-
-		// Get the download ID
-		$dlid = $this->params->get('downloadid', '');
-
-		// Appent the Download ID to the download URL
-		if (!empty($dlid))
-		{
-			$uri->setVar('key', $dlid);
-			$url = $uri->toString();
-		}
-		
-		return true;
-	}
 }
